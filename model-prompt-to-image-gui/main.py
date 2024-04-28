10f7a9995c8bb0d00db6ec97ffb0ace6f1018135
chamaael@etu.unistra.fr <chamaael@etu.unistra.fr>
2024-04-29 01:13:53 +0200

    Many improvements changed used model and ui improvements

--- model-prompt-to-image-gui/main.py
+++ model-prompt-to-image-gui/main.py
@@ -18,26 +18,32 @@ class GUI(tkinter.Tk):
         'add_watermarker': False,
         'variant': "fp16"
     }
-
-    txt_model: sdk.AjDudeDialogptSmallHarrypotter
+    txt_model: sdk.Fredzhang7AnimeAnythingPromptgenV2
     txt_model_options = {
     }
+    model_manager_img = sdk.ModelsManagement()
+    model_manager_txt = sdk.ModelsManagement()
 
     def __init__(self):
         super().__init__()
+        self.bind('<Return>', lambda event: self.start_generation())
 
-    def load(self):
+    def load_img(self):
         """
         Load the models
         """
-        self.progress_bar.start(10)
-        self.img_model = sdk.StabilityaiStableDiffusionXlBase10(**self.img_model_options)
-        self.img_model.load_model()
-        self.progress_bar.stop()
 
         self.progress_bar.start(10)
-        self.txt_model = sdk.AjDudeDialogptSmallHarrypotter(**self.txt_model_options)
-        self.txt_model.load_model()
+        self.img_model = sdk.StabilityaiStableDiffusionXlBase10(**self.img_model_options)
+        self.model_manager_img.add_model(new_model=self.img_model)
+        self.model_manager_img.load_model(self.img_model.model_name)
+        self.progress_bar.stop()
+
+    def load_txt(self):
+        self.progress_bar.start(10)
+        self.txt_model = sdk.Fredzhang7AnimeAnythingPromptgenV2(**self.txt_model_options)
+        self.model_manager_txt.add_model(new_model=self.txt_model)
+        self.model_manager_txt.load_model(self.txt_model.model_name)
         self.progress_bar.stop()
 
         self.enable_input()
@@ -52,8 +58,14 @@ class GUI(tkinter.Tk):
         self.disable_input()
         prompt = " Instruct: " + self.textbox.get() + ".\nOutput:"
 
-        conversation = self.txt_model.generate_prompt(prompt=prompt, max_new_tokens=100)
+        conversation = self.model_manager_txt.generate_prompt(prompt=prompt, max_length=76,
+                                                              num_return_sequences=10, do_sample=True,
+                                                              repetition_penalty=1.2, temperature=0.7, top_k=4,
+                                                              early_stopping=True, num_beams=20,
+                                                              truncation=True)
+        print("conv", conversation)
         generated_text = conversation[0]['generated_text']
+
         # Update the conversation label with the generated text
         self.conv_label.config(text=generated_text)
 
@@ -61,7 +73,10 @@ class GUI(tkinter.Tk):
         self.progress_bar.stop()
 
         self.progress_bar.start(10)
-        img = self.img_model.generate_prompt(prompt=generated_text, height=512, width=512)[0]
+        self.run_img()
+        output = generated_text.split('\nOutput: ')[1]
+
+        img = self.model_manager_img.generate_prompt(prompt=output, height=512, width=512)[0]
 
         tkimg = ImageTk.PhotoImage(img[0])
         self.image_label.config(image=tkimg)
@@ -97,23 +112,27 @@ class GUI(tkinter.Tk):
         """
         self.title("Demo 3: text prompt to image")
 
+        # Adjusting dimensions of the main application window
+        self.geometry("1200x800")  # Adjusted dimensions
+
         # Frame for the conversation display
-        self.conversation_frame = ttk.Frame(self, width=600, height=400, relief="solid")  # Adjusted dimensions
+        self.conversation_frame = ttk.Frame(self, width=1000, height=400, relief="solid")  # Adjusted dimensions
         self.conversation_frame.pack(padx=10, pady=10)
 
-        self.conv_label = ttk.Label(self.conversation_frame, wraplength=600)  # Added wraplength to handle long text
+        self.conv_label = ttk.Label(self.conversation_frame, wraplength=1000,
+                                    anchor='center')  # Added wraplength and anchor
         self.conv_label.pack(padx=10, pady=10)
 
         # Frame for the image placeholder
-        self.image_frame = ttk.Frame(self, width=300, height=200, relief="solid")
+        self.image_frame = ttk.Frame(self, width=1000, height=400, relief="solid")  # Adjusted dimensions
         self.image_frame.pack(padx=10, pady=10)
 
         # Create a progress bar
-        self.progress_bar = ttk.Progressbar(self, mode='indeterminate', )
+        self.progress_bar = ttk.Progressbar(self, mode='indeterminate')
         self.progress_bar.pack(pady=5)
 
         # Label asking for input
-        self.input_label = ttk.Label(self, text="Enter your text prompt:")
+        self.input_label = ttk.Label(self, text="Enter your text prompt:", anchor='center')  # Added anchor
         self.input_label.pack(padx=10, pady=5)
 
         # Placeholder for the image
@@ -132,20 +151,27 @@ class GUI(tkinter.Tk):
 
         sv_ttk.set_theme("dark")
 
-
-    def run(self):
+    def run_txt(self):
         """
         Run the GUI
         """
         # Create and start a new thread to load the model
-        loading_thread = threading.Thread(target=self.load)
+        loading_thread = threading.Thread(target=self.load_txt())
         self.after(300, lambda: loading_thread.start())
 
         # Run main loop
         self.mainloop()
 
+    def run_img(self):
+        """
+        Run the GUI
+        """
+        # Create and start a new thread to load the model
+        loading_thread = threading.Thread(target=self.load_img())
+        self.after(300, lambda: loading_thread.start())
+
 
 if __name__ == '__main__':
     gui = GUI()
     gui.setup()
-    gui.run()
+    gui.run_txt()
